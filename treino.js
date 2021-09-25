fdc60b423205ad693ad99f7091a8a0ae81325494
SeverinoArmando <severinoarmando3@gmail.com>
2021-09-25 00:38:59 -0300

    exercicio

---
+++ treino.js
@@ -0,0 +1,26 @@
+//criando um condigo que calucla a média do aluno
+
+
+
+
+
+
+
+
+let nota1 = 3
+let nota2 = 6
+let nota3 = 7
+let media = (nota1+nota2+nota3)/3
+
+
+if(media<=5){
+    console.log(`Aluno reprovado com a média de: ${media}`)
+}else{
+    console.log(`Aluno Aprovado com a nota de: ${media}`)
+}
+
+
+ function soma(a,b){
+    return a + b;
+}
+console.log(soma(4,7))
